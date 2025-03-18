00c74399e29c6bf4d862510d5f295ccc50fe9459
aneetan <anitaneupane2061@gmail.com>
2025-03-18 19:04:57 +0545

    Guide cards

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -8,6 +8,7 @@ import Signin from './pages/authenticate/Signin';
 import Login from './pages/authenticate/Login';
 import Signup from './pages/authenticate/Signup';
 import Admin from './pages/admin/Admin';
+import ViewGuides from './pages/user/ViewGuides';
 
 
 
@@ -21,6 +22,8 @@ function App() {
             <Route path="/" element={<LandingPage />} />
             <Route path='/seeDestination' element={<CustomMap/>}/>
             <Route path='/seeMore' element={<SeeDestinations/>}/>
+            <Route path='/viewGuides' element={<ViewGuides/>}/>
+            <Route path='/bookGuides' element={<ViewGuides/>}/>
           </Route>
           <Route path='/login' element={<Signin/>}/>
           <Route path='/register' element={<Signup/>}/>
---
+++ react-project/src/pages/user/ViewGuides.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import GuideSection from './GuideSection'
+
+const ViewGuides = () => {
+  return (
+    <>
+        <main>
+            <GuideSection/>
+        </main>
+      
+    </>
+  )
+}
+
+export default ViewGuides
